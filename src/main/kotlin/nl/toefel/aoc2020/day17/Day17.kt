65b854e60d0926abb0febb4be87cf7c14781feea
Christophe Hesters <toefel18@gmail.com>
2020-12-19 17:04:04 +0100

    Day17 part 2

--- src/main/kotlin/nl/toefel/aoc2020/day17/Day17.kt
+++ src/main/kotlin/nl/toefel/aoc2020/day17/Day17.kt
@@ -1,62 +1,59 @@
 package nl.toefel.aoc2020.day17
 
 import java.io.File
-import java.lang.ClassLoader.getSystemResource
-import kotlin.math.min
-import kotlin.math.max
 
-data class Point4D(val x: Int, val y: Int, val z: Int) {
-
+interface Point {
+    val withNeighbors: List<Point>
+    val neighbors: List<Point>
 }
 
+interface IPoint3D : Point{
+    val x : Int
+    val y : Int
+    val z : Int
+}
 
-data class Point3D(val x: Int, val y: Int, val z: Int) {
-    val withNeighbors: List<Point3D> by lazy {
+data class Point3D(override val x: Int, override val y: Int, override val z: Int) : IPoint3D{
+    override val withNeighbors: List<Point3D> by lazy {
         (x - 1..x + 1).flatMap { iX -> (y - 1..y + 1).flatMap { iY -> (z - 1..z + 1).map { iZ -> Point3D(iX, iY, iZ) } } }
     }
-    val neighbors: List<Point3D> get() = withNeighbors - this
+    override val neighbors: List<Point3D> get() = withNeighbors - this
+}
+
+data class Point4D(val point3d: Point3D, val w: Int) : IPoint3D by point3d{
+    override val withNeighbors: List<Point4D> by lazy {
+        (w - 1..w +1).flatMap { iW -> point3d.withNeighbors.map { Point4D(it, iW) }}
+    }
+    override val neighbors: List<Point4D> get() = withNeighbors - this
 }
 
 data class Cube(val active: Boolean) {
     override fun toString(): String = if (active) "#" else "."
 }
 
-class Universe(val map: Map<Point3D, Cube>, val cycle: Int = 0) : Map<Point3D, Cube> by map {
-    val minBound: Point3D get() = keys.reduce { acc, next -> Point3D(min(acc.x, next.x), min(acc.y, next.y), min(acc.z, next.z)) }
-    val maxBound: Point3D get() = keys.reduce { acc, next -> Point3D(max(acc.x, next.x), max(acc.y, next.y), max(acc.z, next.z)) }
-
-    fun print() {
-        val (min, max) = (minBound to maxBound)
-        val zSlices = (min.z..max.z).map { z ->
-            (min.y..max.y).joinToString("\n") { y ->
-                (min.x..max.x).joinToString("") { x ->
-                    this.getOrDefault(Point3D(x, y, z), Cube(false)).toString()
-                }
-            }.let { "z=$z\n$it" }
-        }
-        println("cycle $cycle, alive = ${values.count { it.active }}")
-        zSlices.map { it.split('\n') }
-                .reduce { acc, slice -> acc.zip(slice).map { "${it.first.padEnd(max.x - min.x + 2)} ${it.second.padEnd(max.x - min.x + 2)}" } }
-                .forEach { println(it) }
-    }
-
-    fun nextCycle(): Universe {
+class Universe<T : Point>(val map: Map<T, Cube>, val cycle: Int = 0) : Map<T, Cube> by map {
+    fun nextCycle(): Universe<T> {
         return keys.flatMap { it.withNeighbors }
                 .toSet()
-                .associateWith { pos: Point3D ->
-                    val active = this[pos]?.active == true
-                    val activeNeighbors = pos.neighbors.count { this[it]?.active == true }
+                .associateWith { point ->
+                    val active = this[point]?.active == true
+                    val activeNeighbors = point.neighbors.count { this[it]?.active == true }
                     Cube((active && (activeNeighbors == 2 || activeNeighbors == 3)) || !active && activeNeighbors == 3)
                 }
-                .let { Universe(it, cycle = cycle + 1) }
+                .let { Universe(it as Map<T, Cube>, cycle = cycle + 1) }
     }
 }
 
 fun main() {
-    val input = File(getSystemResource("day17.txt").file).readLines()
+    val input3D = File(ClassLoader.getSystemResource("day17.txt").file).readLines()
             .flatMapIndexed { y, row -> row.mapIndexed { x, state -> Point3D(x, y, 0) to Cube(state == '#') } }
             .toMap()
 
-    val life = generateSequence(Universe(input)) { it.nextCycle() }
-    life.take(7).forEach { it.print() }
+    val life = generateSequence(Universe(input3D)) { it.nextCycle() }
+    life.take(7).forEach { universe -> println(universe.values.count { it.active }) }
+
+
+    val input4D = input3D.mapKeys { Point4D(it.key, 0) }
+    val life4D = generateSequence(Universe(input4D)) { it.nextCycle() }
+    life4D.take(7).forEach { universe -> println(universe.values.count { it.active }) }
 }
